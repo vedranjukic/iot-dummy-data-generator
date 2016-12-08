c9fdda229533534c531c4bb688ff677c03fbdf4a
Vedran Jukic <vedran@codeanywhere.net>
2016-12-08 16:09:55 +0100

    index.js moved to project root

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-require('./src')
+const DeviceManager = require('./src/fake-device-manager')
+const FakeDeviceConnection = require('./src/fake-device-connection')()
+
+const updateStats = require('./src/stats')()
+
+console.log('Generating data - send interval: 15s')
+
+let deviceManager = DeviceManager({
+  deviceCount: 100,
+  connection: FakeDeviceConnection,
+  onStats: updateStats,
+  measurements: {
+    usage: {type: 'integer', min: 0, max: 100},
+    voltage: {type: 'integer', min: 0, max: 15},
+    temperature: {type: 'integer', min: -20, max: 60}
+  }
+})
+
+deviceManager.start()
